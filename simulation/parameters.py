d2c4e929be46e11c82d7b62bc62ed67147f99b5d
amyheather <a.heather2@exeter.ac.uk>
2025-03-28 15:42:29 +0000

    feat(parameters): add RestrictAttributesMeta and RestrictAttributes

--- simulation/parameters.py
+++ simulation/parameters.py
@@ -6,7 +6,63 @@ probabilities between different care settings.
 """
 
 
-class ASUArrivals:
+class RestrictAttributesMeta(type):
+    """
+    Metaclass for attribute restriction.
+
+    A metaclass modifies class construction. It intercepts instance creation
+    via __call__, adding the _initialised flag after __init__ completes. This
+    is later used by RestrictAttributes to enforce attribute restrictions.
+    """
+    def __call__(cls, *args, **kwargs):
+        # Create instance using the standard method
+        instance = super().__call__(*args, **kwargs)
+        # Set the '_initialised' flag to True, marking end of initialisation
+        instance.__dict__['_initialised'] = True
+        return instance
+
+
+class RestrictAttributes(metaclass=RestrictAttributesMeta):
+    """
+    Base class that prevents the addition of new attributes after
+    initialisation.
+
+    This class uses RestrictAttributesMeta as its metaclass to implement
+    attribute restriction. It allows for safe initialisation of attributes
+    during the __init__ method, but prevents the addition of new attributes
+    afterwards.
+
+    The restriction is enforced through the custom __setattr__ method, which
+    checks if the attribute already exists before allowing assignment.
+    """
+    def __setattr__(self, name, value):
+        """
+        Prevent addition of new attributes.
+
+        Arguments:
+            name (str):
+                The name of the attribute to set.
+            value (Any):
+                The value to assign to the attribute.
+
+        Raises:
+            AttributeError:
+                If `name` is not an existing attribute and an attempt is made
+                to add it to the class instance.
+        """
+        # Check if the instance is initialised and the attribute doesn't exist
+        if hasattr(self, '_initialised') and not hasattr(self, name):
+            # Get a list of existing attributes for the error message
+            existing = ', '.join(self.__dict__.keys())
+            raise AttributeError(
+                f'Cannot add new attribute "{name}" - only possible to ' +
+                f'modify existing attributes: {existing}.'
+            )
+        # If checks pass, set the attribute using the standard method
+        object.__setattr__(self, name, value)
+
+
+class ASUArrivals(RestrictAttributes):
     """
     Arrival rates for the acute stroke unit (ASU) by patient type.
 
@@ -31,7 +87,7 @@ class ASUArrivals:
         self.other = other
 
 
-class RehabArrivals:
+class RehabArrivals(RestrictAttributes):
     """
     Arrival rates for the rehabiliation unit by patient type.
 
@@ -53,7 +109,7 @@ class RehabArrivals:
         self.other = other
 
 
-class ASULOS:
+class ASULOS(RestrictAttributes):
     """
     Mean and standard deviation (SD) of length of stay (LOS) in days in the
     acute stroke unit (ASU) by patient type.
@@ -99,7 +155,7 @@ class ASULOS:
         self.other_sd = other_sd
 
 
-class RehabLOS:
+class RehabLOS(RestrictAttributes):
     """
     Mean and standard deviation (SD) of length of stay (LOS) in days in the
     rehabilitation unit by patient type.
@@ -145,7 +201,7 @@ class RehabLOS:
         self.other_sd = other_sd
 
 
-class ASURouting:
+class ASURouting(RestrictAttributes):
     """
     Probabilities of each patient type being transferred from the acute
     stroke unit (ASU) to other destinations.
@@ -198,7 +254,7 @@ class ASURouting:
         self.other_other = other_other
 
 
-class RehabRouting:
+class RehabRouting(RestrictAttributes):
     """
     Probabilities of each patient type being transferred from the rehabiliation
     unit to other destinations.
@@ -238,7 +294,7 @@ class RehabRouting:
         self.other_other = other_other
 
 
-class Param:
+class Param(RestrictAttributes):
     """
     Default parameters for simulation.
     """
@@ -253,7 +309,7 @@ class Param:
     ):
         """
         Initialise a parameter set for the simulation.
-        
+
         Arguments:
             asu_arrivals (ASUArrivals):
                 Arrival rates to the acute stroke unit (ASU).
