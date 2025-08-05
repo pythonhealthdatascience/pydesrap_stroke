72cae9249f45bfed6a97f5ebd092808dddd72d47
amyheather <a.heather2@exeter.ac.uk>
2025-08-05 14:50:52 +0100

    refactor(parameters): Use parameters.json and sim-tools.DistributionRegistry. Remove all individual parameter classes and validation. Move RestrictAttributes to separate file. (#16)

--- simulation/model.py
+++ simulation/model.py
@@ -4,7 +4,7 @@ Code for the simulation.
 
 import numpy as np
 import simpy
-from sim_tools.distributions import Exponential, DiscreteEmpirical, Lognormal
+from sim_tools.distributions import DistributionRegistry
 
 
 class Patient:
@@ -110,88 +110,19 @@ class Model:
         self.rehab_occupancy = 0
         self.audit_list = []
 
-        # Create seeds, spawning enough for all the sampling distributions
-        ss = np.random.SeedSequence(entropy=self.run_number)
-        self.seed_generator = iter(ss.spawn(30))
-
-        # Create arrival sampling distributions
-        self.arrival_dist = self.create_distributions(
-            asu_param=self.param.asu_arrivals,
-            rehab_param=self.param.rehab_arrivals,
-            distribution_type="exponential")
-
-        # Create routing sampling distributions
-        self.routing_dist = self.create_distributions(
-            asu_param=self.param.asu_routing,
-            rehab_param=self.param.rehab_routing,
-            distribution_type="discrete")
-
-        # Create length of stay (LOS) sampling distributions
-        self.los_dist = self.create_distributions(
-            asu_param=self.param.asu_los,
-            rehab_param=self.param.rehab_los,
-            distribution_type="lognormal")
-
-    def create_distributions(self, asu_param, rehab_param, distribution_type):
-        """
-        Create a nested dictionary with two items: "asu" and "rehab". Each
-        then contains a set of distributions by patient type.
-
-        Parameters
-        ----------
-        asu_param: Class
-            Acute stroke unit (ASU) parameters.
-        rehab_param: Class
-            Rehabilitation unit parameters.
-        distribution_type: str
-            Name of the distribution to use ("exponential", "discrete",
-            "lognormal").
-
-        Returns
-        -------
-        distributions: dictionary
-            Dictionary with "asu" and "rehab", which each then contains a
-            dictionary of sampling distributions by patient type.
-        """
-        # Create dictionary to store distributions
-        distributions = {}
-
-        # Loop through each unit
-        for unit, unit_param in [("asu", asu_param), ("rehab", rehab_param)]:
-
-            # Make sub-dictionary to store that unit's distributions
-            distributions[unit] = {}
-
-            # Get a list of the patients in that unit (ignore other attributes)
-            patient_types = [attr for attr in dir(unit_param) if attr in
-                             ["stroke", "stroke_esd", "stroke_noesd",
-                              "stroke_mortality", "tia", "neuro", "other"]]
-
-            # For each patient type...
-            for patient_type in patient_types:
-
-                # Get the entry for that patient
-                patient_param = getattr(unit_param, patient_type)
-
-                # Create distributions of specified type
-                if distribution_type == "exponential":
-                    distributions[unit][patient_type] = Exponential(
-                        mean=patient_param,
-                        random_seed=next(self.seed_generator)
-                    )
-                elif distribution_type == "discrete":
-                    distributions[unit][patient_type] = DiscreteEmpirical(
-                        values=list(patient_param.keys()),
-                        freq=list(patient_param.values()),
-                        random_seed=next(self.seed_generator)
-                    )
-                elif distribution_type == "lognormal":
-                    distributions[unit][patient_type] = Lognormal(
-                        mean=patient_param["mean"],
-                        stdev=patient_param["sd"],
-                        random_seed=next(self.seed_generator)
-                    )
-        return distributions
+        # Create all the distributions
+        flat_dist = DistributionRegistry.create_batch(
+            self.param.dist_config, main_seed=self.run_number
+        )
+        # Restructure as dist[type][unit][patient]
+        self.dist = {}
+        for key, obj in flat_dist.items():
+            parts = key.split('_')
+            unit = parts[0]
+            dist_type = parts[1]
+            patient = '_'.join(parts[2:])
+            self.dist.setdefault(dist_type, {}) \
+                .setdefault(unit, {})[patient] = obj
 
     def patient_generator(self, patient_type, unit):
         """
@@ -206,7 +137,7 @@ class Model:
         """
         while True:
             # Sample and pass time to arrival
-            sampled_iat = self.arrival_dist[unit][patient_type].sample()
+            sampled_iat = self.dist["arrival"][unit][patient_type].sample()
             yield self.env.timeout(sampled_iat)
 
             # Create a new patient and add to the patients list
@@ -245,7 +176,7 @@ class Model:
         # Sample destination after ASU (we do this immediately on arrival
         # in the ASU, as the destination influences the length of stay)
         patient.post_asu_destination = (
-            self.routing_dist["asu"][patient.patient_type].sample())
+            self.dist["routing"]["asu"][patient.patient_type].sample())
 
         # Log the post-ASU destination
         self.param.logger.log(
@@ -270,7 +201,7 @@ class Model:
             routing_type = patient.patient_type
 
         # Sample LOS on the ASU, log it and pass time
-        patient.asu_los = self.los_dist["asu"][routing_type].sample()
+        patient.asu_los = self.dist["los"]["asu"][routing_type].sample()
         self.param.logger.log(
             sim_time=self.env.now,
             msg=(f"Patient {patient.patient_id} ({patient.patient_type}) " +
@@ -302,7 +233,7 @@ class Model:
 
         # Sample destination after rehab
         patient.post_rehab_destination = (
-            self.routing_dist["rehab"][patient.patient_type].sample())
+            self.dist["routing"]["rehab"][patient.patient_type].sample())
 
         # Log the post-rehab destination
         self.param.logger.log(
@@ -321,7 +252,7 @@ class Model:
             routing_type = patient.patient_type
 
         # Sample LOS on the rehab unit, log it and pass time
-        patient.rehab_los = self.los_dist["rehab"][routing_type].sample()
+        patient.rehab_los = self.dist["los"]["rehab"][routing_type].sample()
         self.param.logger.log(
             sim_time=self.env.now,
             msg=(f"Patient {patient.patient_id} ({patient.patient_type}) " +
@@ -388,18 +319,6 @@ class Model:
         self.param.logger.log(vars(self.param))
         self.param.logger.log(msg="Logger:\n ")
         self.param.logger.log(vars(self.param.logger))
-        self.param.logger.log(msg="ASU arrivals:\n ")
-        self.param.logger.log(vars(self.param.asu_arrivals))
-        self.param.logger.log(msg="ASU LOS:\n ")
-        self.param.logger.log(vars(self.param.asu_los))
-        self.param.logger.log(msg="ASU routing:\n ")
-        self.param.logger.log(vars(self.param.asu_routing))
-        self.param.logger.log(msg="Rehab arrivals:\n ")
-        self.param.logger.log(vars(self.param.rehab_arrivals))
-        self.param.logger.log(msg="Rehab LOS:\n ")
-        self.param.logger.log(vars(self.param.rehab_los))
-        self.param.logger.log(msg="Rehab routing:\n ")
-        self.param.logger.log(vars(self.param.rehab_routing))
 
         # Calculate the total run length
         run_length = (self.param.warm_up_period +
@@ -407,7 +326,7 @@ class Model:
 
         # Schedule patient generators to run during the simulation
         # For each unit...
-        for unit, patient_types in self.arrival_dist.items():
+        for unit, patient_types in self.dist["arrival"].items():
             # For each patient...
             for patient_type, _ in patient_types.items():
                 self.env.process(
